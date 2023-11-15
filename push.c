9875ad103606c1a101eb036901700d7bc8b248bf
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-15 15:57:41 -0800

    a

--- push.c
+++ push.c
@@ -7,14 +7,14 @@
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-int a, b = 0, flag = 0;
+int n, j = 0, flag = 0;
 if (bus.arg)
 {
 if (bus.arg[0] == '-')
-b++;
-for (; bus.arg[b] != '\0'; b++)
+j++;
+for (; bus.arg[j] != '\0'; j++)
 {
-if (bus.arg[b] > 57 || bus.arg[b] < 48)
+if (bus.arg[j] > 57 || bus.arg[j] < 48)
 flag = 1; }
 if (flag == 1)
 { fprintf(stderr, "L%d: usage: push integer\n", counter);
@@ -28,9 +28,9 @@ fclose(bus.file);
 free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE); }
-a = atoi(bus.arg);
+n = atoi(bus.arg);
 if (bus.lifi == 0)
-addnode(head, a);
+addnode(head, n);
 else
-addqueue(head, a);
+addqueue(head, n);
 }
