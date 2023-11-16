aa5f15cfe9d63ec3074b4bb60a56054beeda2a69
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-15 16:04:21 -0800

    a

--- push.c
+++ push.c
@@ -8,6 +8,7 @@
 void f_push(stack_t **head, unsigned int counter)
 {
 int a, b = 0, flag = 0;
+
 if (bus.arg)
 {
 if (bus.arg[0] == '-')
@@ -23,7 +24,7 @@ free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE); }}
 else
-{ fprintf(stderr, "L%d: uasge: push integer\n", counter);
+{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 fclose(bus.file);
 free(bus.content);
 free_stack(*head);
