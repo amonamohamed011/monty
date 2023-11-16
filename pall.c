dce94e81a594b9c75c5a8828faa6b23b97f28668
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-15 16:35:11 -0800

    a

--- pall.c
+++ pall.c
@@ -2,13 +2,14 @@
 /**
 * f_pall - prints the stack
 * @head: stack head
-* @counter: no use
+* @counter: no used
 * Return: no return
 */
 void f_pall(stack_t **head, unsigned int counter)
 {
 stack_t *h;
 (void)counter;
+
 h = *head;
 if (h == NULL)
 return;
